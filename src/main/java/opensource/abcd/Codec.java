7d26a34936e8451e614ffcc9f46e97ac43fe8701
郭春阳 <guochunyang@qiyi.com>
2017-06-07 20:39:02 +0800

    update

---
+++ src/main/java/opensource/abcd/Codec.java
@@ -0,0 +1,15 @@
+package opensource.abcd;
+
+import java.io.UnsupportedEncodingException;
+
+public class Codec {
+	public static String encodeStr(String str) {
+		try {
+			return new String(str.getBytes("ISO-8859-1"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+}
--- src/main/java/opensource/abcd/Map2Bean.java
+++ src/main/java/opensource/abcd/Map2Bean.java
@@ -1,10 +1,12 @@
 package opensource.abcd;
 
 
+import java.util.List;
 import java.util.Map;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Set;
+import java.util.Arrays;
 import java.util.Iterator;
 
 public class Map2Bean {
@@ -19,7 +21,13 @@ public class Map2Bean {
                 String mn = ms[i].getName();
                 if(mn.contains("set") && mn.toLowerCase().contains(ele)){
                     try {
-                        ms[i].invoke(ob, map.get(ele)[0]);
+                        Class<?> clazz = ms[i].getParameterTypes()[0];
+                        if(clazz.isAssignableFrom(List.class)){
+                            String[] res = map.get(ele);
+                            ms[i].invoke(ob, (Object)Arrays.asList(res));
+                        }else if(clazz.isAssignableFrom(String.class)){
+                            ms[i].invoke(ob, Codec.encodeStr(map.get(ele)[0]));
+                        }
                     } catch (IllegalAccessException e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
